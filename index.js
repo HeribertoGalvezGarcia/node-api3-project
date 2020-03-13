ff6c45ead6f88bef5ecf49191b2b61eb79470001
HeribertoGalvezGarcia <heriberto.galvezgarcia@gmail.com>
2020-03-12 20:56:19 -0700

    Make app work for heroku

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const server = require('./server');
 const userRouter = require('./users/userRouter');
 const postRouter = require('./posts/postRouter');
@@ -5,5 +7,5 @@ const postRouter = require('./posts/postRouter');
 server.use('/api/users', userRouter);
 server.use('/api/posts', postRouter);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`Running API on port ${port}`));
